83db7abeee137c62ba8d677dcd8a5961100a3d0b
Katie Palof <katie.palof@alaska.gov>
2017-08-31 10:09:22 -0800

    replicate figure 2 in memo for reference

--- code/regional_biomass_threshold_explore.R
+++ code/regional_biomass_threshold_explore.R
@@ -20,3 +20,25 @@ theme_set(theme_bw(base_size=12,base_family='Times New Roman')+
 #Load data ----------------
 
 biomass <- read_excel(path = "./data/regional_biomass.xlsx")
+
+### regional figure ------------
+# replication of Figure 2 from 2017 memo
+biomass %>% select(Year, legal, mature) ->biomass1
+biomass1_long <- gather(biomass1, type, pounds, legal:mature, factor_key = TRUE)
+
+ggplot(biomass1_long, aes(Year, pounds, group = type))+ 
+  geom_point(aes(color = type, shape = type), size =3) +
+  geom_line(aes(color = type, group = type))+
+  scale_colour_manual(name = "", values = c("grey1", "grey1"))+
+  scale_shape_manual(name = "", values = c(16, 1))+
+  
+  ylim(0,2000000) +ggtitle("regional biomass") + ylab("Biomass (lbs)")+ xlab("")+
+  theme(axis.text.x = element_blank(), plot.title = element_text(hjust =0.5)) + 
+  #scale_x_continuous(breaks = seq(min(1993),max(2017), by =2)) +
+  theme(legend.position = c(0.8,0.7)) + 
+  geom_hline(yintercept = 761292, color = "grey1")+
+  geom_hline(yintercept = 1117286, color = "grey1", linetype = "dashed")
+
+biomass %>% filter(Year >= 1993 & Year <= 2007) %>% summarise(mean(legal))
+biomass %>% filter(Year >= 1993 & Year <= 2007) %>% summarise(mean(mature))
+
